1d928b3588af99fb1dbbd46964e8161295b50cb4
Fedor Baart <fedor.baart@deltares.nl>
2017-08-11 12:06:46 +0200

    fix and add test

---
+++ tests/test_datasets.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `coastviewer` package."""
+
+import pytest
+
+from coastviewer import datasets
+
+TRANSECTS = [7003900, 4000100]
+
+def test_get_transect_data():
+    """Test the CLI."""
+    for id_ in TRANSECTS:
+        data = datasets.get_transect_data(id_)
