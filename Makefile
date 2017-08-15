c8cc182fe119c32e28ef3f237cf5f93c8b18b81d
Fedor Baart <fedor.baart@deltares.nl>
2017-08-15 10:10:13 +0200

    test performance

--- Makefile
+++ Makefile
@@ -53,6 +53,9 @@ lint: ## check style with flake8
 test: ## run tests quickly with the default Python
 	py.test
 
+test-profile: ## show slowest 3 tests
+	py.test --duration=3
+
 
 test-all: ## run tests on every Python version with tox
 	tox
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -9,7 +9,13 @@ from coastviewer import datasets
 
 TRANSECTS = [7003900, 4000100]
 
+
 def test_get_transect_data():
     """Test the CLI."""
     for id_ in TRANSECTS:
         data = datasets.get_transect_data(id_)
+
+
+def test_get_nourishment_grid_df():
+    for id_ in TRANSECTS:
+        data = datasets.get_nourishment_grid_df()
