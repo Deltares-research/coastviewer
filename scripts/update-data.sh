0465a826b112452d0f98acc325201fc8c73af492
Fedor Baart <fedor.baart@deltares.nl>
2018-07-17 14:29:15 +0200

    update data script

--- scripts/update-data.sh
+++ scripts/update-data.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
+echo "current directory: $(pwd)"
 
 # Mount EFS if set
 if [ -n $EFS ]
 then
     mkdir /tmp/efs
-    mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=15,retrans=2 ${EFS}:/ /tmp/efs
+    sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=15,retrans=2 ${EFS}:/ /tmp/efs
     cd /tmp/efs
 fi
+
+echo "current directory: $(pwd)"
 
 # Download data
 if [[ -d /data ]]
 then
     cd /data
+    echo "running Makefile"
+    cat /app/data/Makefile
     make -f /app/data/Makefile clean
     make -f /app/data/Makefile
     cd -
 else
     cd data
+    echo "running Makefile"
+    cat Makefile
     make clean
     make
     cd -
