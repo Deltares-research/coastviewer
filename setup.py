2d18c0bd0552b3cc9571f326c24e9dd6b1b5f9ac
Fedor Baart <fedor.baart@deltares.nl>
2021-12-21 09:01:50 +0100

    Bump version: 0.1.15 → 0.1.16

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='coastviewer',
-    version='0.1.15',
+    version='0.1.16',
     description="Coast Viewer, the backend for the coast viewer. A tool to visualize coastal data.",
     long_description=readme + '\n\n' + history,
     author="Fedor Baart",
